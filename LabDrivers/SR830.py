5df9727a7e1999efb150eddb27626c670db095cb
Benjamin Schmidt <schmidtb@physics.mcgill.ca>
2017-02-07 21:58:16 -0500

    Improve documentation of SR830 module

--- LabDrivers/SR830.py
+++ LabDrivers/SR830.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 #import visa
+
+from collections import OrderedDict
+from numpy import nan
+
 try:
     from . import Tool
 except:
     import Tool
-from collections import OrderedDict
-from numpy import nan
+
 
 # use an ordered dictionary so that the parameters show up in a pretty order :)
 param = OrderedDict([('X', 'V'), ('Y', 'V'), ('R', 'V'), ('PHASE', 'degrees'),
@@ -17,11 +20,18 @@ INTERFACE = Tool.INTF_GPIB
 import logging
 
 class Instrument(Tool.MeasInstr):
-
+    """ Class to communicate with Stanford Research Systems SR830 lock-in"""
+    
     def __init__(self, resource_name, debug = False, **kwargs):
         super(Instrument, self).__init__(resource_name, 'SR830', debug=debug, interface=INTERFACE, **kwargs)
 
     def measure(self, channel):
+        """ Measure the specified 'channel' and return the result. The list
+        of available channels is given by the keys in param.
+        
+        Args:
+            channel (string): the channel to measure. 
+        """
         if channel in param:
             if channel == 'X':
                 answer = self.read_input(1)
@@ -59,50 +69,73 @@ class Instrument(Tool.MeasInstr):
         return answer
 
     def set_scale(self, scale):
+        """ Set the sensitivity of the input channel. Refer to SR830 
+        documentation for the full list of settings
+        
+        Args:
+            scale (int): Between 0 and 26, the sensitivity to use
+        """
         self.write('SENS ' + str(scale))
 
     def set_ref_internal(self):
+        """ Instruct the lock-in to lock to its internal oscillator"""
         self.write('FMOD 1')
 
     def set_ref_external(self):
+        """ Instruct the lock-in to lock to the ref-in input """
         self.write('FMOD 0')
 
     def set_phase(self, shift):
+        """ Set the phase offset.
+        
+        Args:
+            shift (float): Phase shift in degrees
+        """
         self.write('PHAS ' + str(shift))
 
     def set_amplitude(self, amplitude):
+        """ set the reference output amplitude in volts
+        
+        Args:
+            amplitude (float): RMS voltage output of the reference channel
+        """
         self.write('SLVL' + str(amplitude))
 
     def get_amplitude(self):
+        """ Query the current reference channel output amplitude """
         if not self.DEBUG:
             return self.ask('SLVL?')
         else:
-            return 1.0
+            return nan
 
     def set_freq(self, freq):
+        """ Set the frequency of the reference channel.
+        
+        Args: 
+            freq (float): frequency in Hertz. 
+        """
         self.write('FREQ ' + str(freq))
 
     def get_freq(self):
+        """ Query the frequency of the reference channel."""
         if not self.DEBUG:
             return float(self.ask('FREQ?'))
 
     def set_harm(self, harm):
+        """ Sets the harmonic number to measure. Use set_harm(1) to measure
+        at the reference frequency itself.
+        
+        Args:
+            harm (int): harmonic number to measure.
+        """
         self.write('HARM ' + str(harm))
 
-    def set_ref_out(self, voltage):
-        self.write('SLVL ' + str(voltage))
-
-    def get_ref_out(self, voltage):
-        if not self.DEBUG:
-            self.write('SLVL?')
-            return self.read()
-        else:
-            return 1.234
-
     def read_aux_in(self, chan):
-        '''
-        Reads the AuxIn voltage value of the lock-in. The channel values can be 1, 2 ,3 or 4. 
-        '''
+        """ Reads the AuxIn voltage value of the lock-in. 
+        
+        Args: 
+            chan (int): channel to read, one of 1, 2 ,3 or 4. 
+        """
         if not self.debug:
             self.write('OAUX? ' + str(chan))
             return float(self.read())
@@ -110,9 +143,11 @@ class Instrument(Tool.MeasInstr):
             return 1.234
             
     def read_aux_out(self, chan):
-        '''
-        Reads the AuxOut voltage value of the lock-in. The channel values can be 1, 2 ,3 or 4. 
-        '''
+        """ Reads the AuxOut voltage value of the lock-in. 
+        
+        Args: 
+            chan (int): channel to read, one of 1, 2 ,3 or 4. 
+        """
         if not self.debug:
             self.write('AUXV? ' + str(chan))
             return float(self.read())
@@ -120,16 +155,21 @@ class Instrument(Tool.MeasInstr):
             return 1.234
 
     def set_aux_out(self, chan, volts):
-        '''
-        Sets the AuxOut voltage value for the lock-in. The channel values can be 1, 2 ,3 or 4. 
-        '''
+        """ Sets the AuxOut voltage value for the lock-in. 
+        
+        Args: 
+            chan (int): channel to read, one of 1, 2 ,3 or 4. 
+        """
         self.write('AUXV ' + str(chan) + ", " + str(volts))
 
 
     def read_input(self, num):
-        '''
-        Reads the specificed input of the lockin. 1=x, 2=y, 3=r, 4=phase
-        '''
+        """
+        Reads the specificed input of the lockin. 
+        
+        Args:
+            num (int): 1=x, 2=y, 3=r, 4=phase
+        """
         if not self.DEBUG:
             try:
                 return float(self.ask('OUTP? ' + str(num)))
@@ -141,10 +181,8 @@ class Instrument(Tool.MeasInstr):
         else:
             return 1.23e-4
 
-# if run as own program
 if (__name__ == '__main__'):
-
-    lockin1 = Instrument("GPIB0::11")
-    print lockin1.identify()
-    print lockin1.measure('X')  # no averaging
-    lockin1.close()
+    
+    from utils import command_line_test
+    
+    command_line_test(Instrument)
