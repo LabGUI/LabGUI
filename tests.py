37a00f3771cd0fa5d5d40c422760cae6146ce5c8
Shammamah Hossain <shammamah.hossain@gmail.com>
2018-02-09 17:32:57 -0500

    Add test for various problematic script files.

--- tests.py
+++ tests.py
@@ -89,200 +89,224 @@ def relaunch():
     
     
 class LabGuiTest(unittest.TestCase):    
-
-        
-    '''Test the LabGui GUI'''  
+#
+#        
+#    '''Test the LabGui GUI'''  
     def setUp(self):
         '''Create the GUI'''
         self.form = LabGui.LabGuiMain()
         
         self.setting_fname = "test_settings.set"    
 
-    def test_no_plot_window(self):
-        '''Test that there is no window at the start
-        '''
-
-        self.assertRaises(IndexError, self.form.get_last_window())
-
-    def set_simple_instrument_list(self):
-        
-        self.form.widgets['InstrumentWidget'].set_lists(4)
-        
-        instr = ['TIME', 'DICE', 'DICE', 'TIME']
-        port = ['', 'COM2', 'COM3', '']
-        param = ['Time', 'Roll', 'Roll', 'dt']
-        
-        for i in range(len(instr)):
-            
-            cbb = self.form.widgets['InstrumentWidget'].lines[i].instr_name_cbb
-
-            cbb.setCurrentIndex(cbb.findText(instr[i]))
-            
-            cbb = self.form.widgets['InstrumentWidget'].lines[i].port_cbb            
-            
-            cbb.setCurrentIndex(cbb.findText(''))
-            QTest.keyClicks(cbb, port[i])
-            
-            cbb = self.form.widgets['InstrumentWidget'].lines[i].param_cbb            
-            
-            cbb.setCurrentIndex(cbb.findText(param[i]))           
-            print(cbb.currentText())
-        
-    def test_dice_connect_and_play(self):
-        '''test the modification of the instrument list
-        click connect to connect the instrument hub
-        '''
-        
-        self.set_simple_instrument_list()
-    
-        # Push OK with the left mouse button
-        connect_bt = self.form.widgets['InstrumentWidget'].bt_connecthub
-        QTest.mouseClick(connect_bt, Qt.LeftButton)
-
-        self.assertEqual(1,1)        
-        
-    def test_save_instrument_settings(self):
-        print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-        print("\ntest_save_instrument_settings\n")        
-        
-        self.set_simple_instrument_list()
-        
-     
-        
-        self.form.file_save_settings(self.setting_fname)
-        
-        self.assertTrue(os.path.exists(self.setting_fname))
-        
-        ofs = open(self.setting_fname, 'r')
-
-        for line in ofs:
-            print(line)
-        
-    def test_load_instrument_settings(self):
-        print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-      
-        print("\ntest_load_instrument_settings\n")    
-        
-        self.form.file_load_settings(self.setting_fname)  
-        
-#        if os.path.exists(self.setting_fname):  
+#    def test_no_plot_window(self):
+#        '''Test that there is no window at the start
+#        '''
+#
+#        self.assertRaises(IndexError, self.form.get_last_window())
+#
+#    def set_simple_instrument_list(self):
 #        
-#            self.assertIsNotNone(self.form.plot_window_settings)
-        
-        
-        self.assertEqual(self.form.instrument_connexion_setting_fname,
-                     self.setting_fname)
-        
-        self.form.file_load_settings("doesnt_exist.fname")  
-        
-        self.assertIsNone(self.form.plot_window_settings)
-    
-    
-    #Shammamah's tests (incomplete!)
-    # Tests connect button  
-    def test_add_remove(self): 
-        print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-
-        print("\ntest_add_remove\n")
-        
-        remove_bt = self.form.widgets['InstrumentWidget'].bt_remove_last         
-        add_bt = self.form.widgets['InstrumentWidget'].bt_add_line
-        connect_bt = self.form.widgets['InstrumentWidget'].bt_connecthub
-        
-        QTest.mouseClick(add_bt, Qt.LeftButton)
-        QTest.mouseClick(connect_bt, Qt.LeftButton)        
-        QTest.mouseClick(remove_bt, Qt.LeftButton)
-        QTest.mouseClick(connect_bt, Qt.LeftButton)
-        
-        
-    # Config file testing: save config settings -> see if settings are loaded correctly 
-    # check data path, script, settings, datafile, debug mode 
-    def test_config_debug(self):
-        print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-        print("\ntest_config_debug\n")
-        filemenu = self.form.fileMenu
-        optionmenu = self.form.optionMenu
-        change_debug = optionmenu.actions()[1]
-        save_config = filemenu.actions()[4]
-
-        print("\nChanging debug state...")
-        change_debug.trigger()
-        f = open("config.txt","r")
-        lines = f.readlines()
-        for i in range(len(lines)): 
-            if("DEBUG" in lines[i]): 
-                print("config.txt: "+lines[i])
-        f.close()
-        print("\nChanging back debug state...")
-        change_debug.trigger()
-        f = open("config.txt","r")
-        lines = f.readlines()
-        for i in range(len(lines)): 
-            if("DEBUG" in lines[i]): 
-                print("config.txt: "+lines[i])
-        f.close()
-        #save_config.trigger()
-            
-        #a = subprocess.check_output("type logging.conf | findstr 'DEBUG*'")
-        #print(a)     
-        # close LabGui and reopen to see if settings have loaded correctly 
-        # not quite sure how to simulate closing the program   
-
-
-    # check if logger output level is changed correctly 
-    def test_logger_output_level(self):
-        print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-        print("\ntest_logger_output_level\n")
-        save_config = self.form.fileMenu.actions()[4]
-        logmenu = self.form.loggingSubMenu.actions()
-        debug = logmenu[0]
-        info = logmenu[1] 
-        warning = logmenu[2] 
-        error = logmenu[3] 
-        
+#        self.form.widgets['InstrumentWidget'].set_lists(4)
+#        
+#        instr = ['TIME', 'DICE', 'DICE', 'TIME']
+#        port = ['', 'COM2', 'COM3', '']
+#        param = ['Time', 'Roll', 'Roll', 'dt']
+#        
+#        for i in range(len(instr)):
+#            
+#            cbb = self.form.widgets['InstrumentWidget'].lines[i].instr_name_cbb
+#
+#            cbb.setCurrentIndex(cbb.findText(instr[i]))
+#            
+#            cbb = self.form.widgets['InstrumentWidget'].lines[i].port_cbb            
+#            
+#            cbb.setCurrentIndex(cbb.findText(''))
+#            QTest.keyClicks(cbb, port[i])
+#            
+#            cbb = self.form.widgets['InstrumentWidget'].lines[i].param_cbb            
+#            
+#            cbb.setCurrentIndex(cbb.findText(param[i]))           
+#            print(cbb.currentText())
+#        
+#    def test_dice_connect_and_play(self):
+#        '''test the modification of the instrument list
+#        click connect to connect the instrument hub
+#        '''
+#        
+#        self.set_simple_instrument_list()
+#    
+#        # Push OK with the left mouse button
+#        connect_bt = self.form.widgets['InstrumentWidget'].bt_connecthub
+#        QTest.mouseClick(connect_bt, Qt.LeftButton)
+#
+#        self.assertEqual(1,1)        
+#        
+#    def test_save_instrument_settings(self):
+#        print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+#        print("\ntest_save_instrument_settings\n")        
+#        
+#        self.set_simple_instrument_list()
+#        
+#     
+#        
+#        self.form.file_save_settings(self.setting_fname)
+#        
+#        self.assertTrue(os.path.exists(self.setting_fname))
+#        
+#        ofs = open(self.setting_fname, 'r')
+#
+#        for line in ofs:
+#            print(line)
+#        
+#    def test_load_instrument_settings(self):
+#        print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+#      
+#        print("\ntest_load_instrument_settings\n")    
+#        
+#        self.form.file_load_settings(self.setting_fname)  
+#        
+##        if os.path.exists(self.setting_fname):  
+##        
+##            self.assertIsNotNone(self.form.plot_window_settings)
+#        
+#        
+#        self.assertEqual(self.form.instrument_connexion_setting_fname,
+#                     self.setting_fname)
+#        
+#        self.form.file_load_settings("doesnt_exist.fname")  
+#        
+#        self.assertIsNone(self.form.plot_window_settings)
+#    
+#    
+#    #Shammamah's tests (incomplete!)
+#    # Tests connect button  
+#    def test_add_remove(self): 
+#        print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+#
+#        print("\ntest_add_remove\n")
+#        
+#        remove_bt = self.form.widgets['InstrumentWidget'].bt_remove_last         
+#        add_bt = self.form.widgets['InstrumentWidget'].bt_add_line
+#        connect_bt = self.form.widgets['InstrumentWidget'].bt_connecthub
+#        
+#        QTest.mouseClick(add_bt, Qt.LeftButton)
+#        QTest.mouseClick(connect_bt, Qt.LeftButton)        
+#        QTest.mouseClick(remove_bt, Qt.LeftButton)
+#        QTest.mouseClick(connect_bt, Qt.LeftButton)
+#        
+#        
+#    # Config file testing: save config settings -> see if settings are loaded correctly 
+#    # check data path, script, settings, datafile, debug mode 
+#    def test_config_debug(self):
+#        print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+#        print("\ntest_config_debug\n")
+#        filemenu = self.form.fileMenu
+#        optionmenu = self.form.optionMenu
+#        change_debug = optionmenu.actions()[1]
+#        save_config = filemenu.actions()[4]
+#
+#        print("\nChanging debug state...")
+#        change_debug.trigger()
+#        f = open("config.txt","r")
+#        lines = f.readlines()
+#        for i in range(len(lines)): 
+#            if("DEBUG" in lines[i]): 
+#                print("config.txt: "+lines[i])
+#        f.close()
+#        print("\nChanging back debug state...")
+#        change_debug.trigger()
+#        f = open("config.txt","r")
+#        lines = f.readlines()
+#        for i in range(len(lines)): 
+#            if("DEBUG" in lines[i]): 
+#                print("config.txt: "+lines[i])
+#        f.close()
+#        #save_config.trigger()
+#            
+#        #a = subprocess.check_output("type logging.conf | findstr 'DEBUG*'")
+#        #print(a)     
+#        # close LabGui and reopen to see if settings have loaded correctly 
+#        # not quite sure how to simulate closing the program   
+#
+#
+#    # check if logger output level is changed correctly 
+#    def test_logger_output_level(self):
+#        print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+#        print("\ntest_logger_output_level\n")
+#        save_config = self.form.fileMenu.actions()[4]
+#        logmenu = self.form.loggingSubMenu.actions()
+#        debug = logmenu[0]
+#        info = logmenu[1] 
+#        warning = logmenu[2] 
+#        error = logmenu[3] 
+#        
 #        for item in logmenu: 
 #            print(item.iconText())
- 
-        print("\nToggling debug...")
-        debug.trigger() 
-        #save_config.toggle() 
-            
-        print("\nToggling info...")
-        info.trigger() 
-        
-        print("\nToggling warning...")
-        warning.trigger()
-        
-        print("\nToggling error...")
-        error.trigger()
-        
-    def test_relaunch(self) :
+# 
+#        print("\nToggling debug...")
+#        debug.trigger() 
+#        #save_config.toggle() 
+#            
+#        print("\nToggling info...")
+#        info.trigger() 
+#        
+#        print("\nToggling warning...")
+#        warning.trigger()
+#        
+#        print("\nToggling error...")
+#        error.trigger()
+#        
+#    def test_relaunch(self) :
+#        print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+#        print("\ntest_relaunch\n")
+#        relaunch() 
+#        
+#        
+#    # Tests from utils.py
+#    # (raising errors if the functions are copy/pasted into this file; 
+#    #   right now, I just call the test functions in utils.py from here) 
+#    
+#    print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+#    print("\ntest_prologix_controller_creation_with_com\n")
+#    utils.test_prologix_controller_creation_with_com(com_port = None)
+#    
+#    print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+#    print("\ntest_prologix_controller_creation_with_wrong_com\n")
+#    utils.test_prologix_controller_creation_with_no_arg_conflict()
+#    
+#    print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+#    print("\ntest_prologix_controller_creation_with_no_arg_conflict()\n")
+#    utils.test_prologix_controller_creation_with_wrong_com()
+#        
+##Logger output level
+##Change debug mode
+## Test for successful output to console
+#    
+    # check if bad scripts are handled correctly    
+    def test_bad_script(self):
         print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-        print("\ntest_relaunch\n")
-        relaunch() 
+        print("\ntest_bad_script\n")
+        script = self.form.widgets['SciptWidget'].scriptFileLineEdit
         
+        f = open("syntaxError.py", "w+")
+        f.write('''
+                prnt("this script doesn't compile")
+                ''')
+        f.close()
         
-    # Tests from utils.py
-    # (raising errors if the functions are copy/pasted into this file; 
-    #   right now, I just call the test functions in utils.py from here) 
-    
-    print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-    print("\ntest_prologix_controller_creation_with_com\n")
-    utils.test_prologix_controller_creation_with_com(com_port = None)
-    
-    print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-    print("\ntest_prologix_controller_creation_with_wrong_com\n")
-    utils.test_prologix_controller_creation_with_no_arg_conflict()
-    
-    print("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-    print("\ntest_prologix_controller_creation_with_no_arg_conflict()\n")
-    utils.test_prologix_controller_creation_with_wrong_com()
+        badScripts = ["\'%s/notarealscript.py\'"%(os.getcwd()),
+                      "\'%s/notapythonscript.c\'"%(os.getcwd()),
+                      "\'%s/directory/\'"%(os.getcwd()),
+                      "\'   \'",
+                      "\'%s/script_example.py\'"%(os.getcwd())]
         
-#Logger output level
-#Change debug mode
-    # Test for successful output to console
+        for badScriptName in badScripts:
+            print("Testing %s\n"%badScriptName)
+            script.setText(badScriptName)
+            self.form.datataker.run()
+            print()
         
-    
+        os.remove("syntaxError.py")
   
 if __name__ == "__main__":
     
