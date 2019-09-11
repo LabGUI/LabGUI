d9281218bb48af9328bdc84160eb3e9f244e5f39
zackorenberg <zachary.berkson-korenberg@mail.mcgill.ca>
2019-09-11 16:40:13 -0400

    Finished both hotfixes, ran Code->Reformat Code in PyCharm for PEP8

--- LabDrivers/ITC503S.py
+++ LabDrivers/ITC503S.py
@@ -7,78 +7,77 @@ try:
 except:
     import Tool
 
-#param = {'T1': 'K', 'T2': 'K', 'T3': 'K'}
+# param = {'T1': 'K', 'T2': 'K', 'T3': 'K'}
 
 param = {
-    'T_SET' : 'K',
-    'T1' : 'K',
-    'T2' : 'K',
-    'T3' : 'K',
-    'T_ERR' : 'K',
+    'T_SET': 'K',
+    'T1': 'K',
+    'T2': 'K',
+    'T3': 'K',
+    'T_ERR': 'K',
     'HEATER_OP': '%',
     'HEATER_OP': 'V',
-    'GAS_FLOW' : 'arbitrary'
-    """
-    Others include (in this order)
-    Proportional Band
-    Integral Action Time
-    Derivative Action Time
-    Channel 1 Freq
-    Channel 2 Freq
-    Channel 3 Freq
-    """
+    'GAS_FLOW': 'arbitrary'
+                """
+                Others include (in this order)
+                Proportional Band
+                Integral Action Time
+                Derivative Action Time
+                Channel 1 Freq
+                Channel 2 Freq
+                Channel 3 Freq
+                """
 
 }
 
 properties = {
-    'Version':{
-        'type':'text',
-        'range':'',
-        'readonly':True,
+    'Version': {
+        'type': 'text',
+        'range': '',
+        'readonly': True,
     },
     'Auto-Heater': {
-        'type':'bool',
-        'range':True,
+        'type': 'bool',
+        'range': True,
     },
-    'Auto-Gas Flow':{
-        'type':'bool',
-        'range':True,
+    'Auto-Gas Flow': {
+        'type': 'bool',
+        'range': True,
     },
-    'Auto-PID':{
-        'type':'bool',
-        'range':True,
+    'Auto-PID': {
+        'type': 'bool',
+        'range': True,
     },
-    'Locked':{
-        'type':'bool',
-        'range':True,
+    'Locked': {
+        'type': 'bool',
+        'range': True,
     },
-    'Remote':{
-        'type':'bool',
-        'range':True,
+    'Remote': {
+        'type': 'bool',
+        'range': True,
     },
-    'Heater Sensor':{
-        'type':'int',
-        'range':[1,3],
+    'Heater Sensor': {
+        'type': 'int',
+        'range': [1, 3],
     },
-    'Proportional Band':{
-        'type':'float',
-        'range':[0,9999]
+    'Proportional Band': {
+        'type': 'float',
+        'range': [0, 9999]
     },
-    'Integral Action Time':{
-        'type':'float',
-        'range':[0,9999]
+    'Integral Action Time': {
+        'type': 'float',
+        'range': [0, 9999]
     },
-    'Derivative Action Time':{
-        'type':'float',
-        'range':[0,9999]
+    'Derivative Action Time': {
+        'type': 'float',
+        'range': [0, 9999]
     },
-    'Desired Temperature':{
-        'type':'float',
-        'range':[0,99999]
+    'Desired Temperature': {
+        'type': 'float',
+        'range': [0, 99999]
     }
 
 }
-
 
 INTERFACE = Tool.INTF_GPIB
 
@@ -87,14 +86,14 @@ INTERFACE = Tool.INTF_GPIB
 
 class Instrument(Tool.MeasInstr):
 
-    def __init__(self, resource_name, debug=False,**kwargs):
+    def __init__(self, resource_name, debug=False, **kwargs):
         print(kwargs)
         if "interface" in kwargs:
             itfc = kwargs.pop("interface")
         else:
             itfc = INTERFACE
 
-        name='ITC503S'
+        name = 'ITC503S'
         self.identification = "Oxford Instrument Intelligent Temperature Controller 503"
         if "name" in kwargs:
             name = kwargs.pop("name")
@@ -102,21 +101,23 @@ class Instrument(Tool.MeasInstr):
         # there is an error regarding the query: *IDN? posed at the beginning.
         # this error leaves the error message ?*IDN? in the
         is_empty = True
-        super(Instrument, self).__init__(resource_name, name=name, debug=debug, interface = itfc, read_termination = '\r', **kwargs)
+        super(Instrument, self).__init__(resource_name, name=name, debug=debug, interface=itfc, read_termination='\r',
+                                         **kwargs)
 
-        #self.term_chars = '\r'
+        # self.term_chars = '\r'
 
-        #self.term_chars = '\r'
-        #self.connection.read_termination = '\r'
-        self.setControl(False, True) #still works with 0,1; but improper
+        # self.term_chars = '\r'
+        # self.connection.read_termination = '\r'
+        self.setControl(False, True)  # still works with 0,1; but improper
         """ FOR FIRST INSTANCE ONLY
         try:
             self.connection.read()
         except:
             pass #really bad programming technique ik
         """
-        #self.write('$W1000')
-        #print("working") will not get to this point
+        # self.write('$W1000')
+        # print("working") will not get to this point
+
     """
     def measure_old(self, channel): #consider rewriting
         if channel in self.last_measure:
@@ -187,6 +188,7 @@ class Instrument(Tool.MeasInstr):
         
            # set controls for locked (0 = False, 1 = True) and remote (0 = local, 1 = remote)
     """
+
     def measure(self, channel):
         if channel in self.last_measure:
             command = "R" + str(list(param.keys()).index(channel))
@@ -197,8 +199,8 @@ class Instrument(Tool.MeasInstr):
             print("existing channels :", self.channels)
             answer = None
 
+        return float(answer.split("R")[-1])  # last data point incase it reads two
 
-        return float(answer.split("R")[-1]) # last data point incase it reads two
     """
     def setControl_old(self, locked, remote):
         if locked and remote:
@@ -210,6 +212,7 @@ class Instrument(Tool.MeasInstr):
         else:
             self.write('$C2')
     """
+
     def set(self, data):
         # set control
         self.setControl(data['Locked'], data['Remote'])
@@ -263,20 +266,20 @@ class Instrument(Tool.MeasInstr):
             remote = int(remote)
 
         unlocked = not locked
-        commandcode = int( str(int(unlocked)) + str(int(remote)), 2)
-        self.write('$C'+str(commandcode))
+        commandcode = int(str(int(unlocked)) + str(int(remote)), 2)
+        self.write('$C' + str(commandcode))
+
     def getControl(self):
         C = self.Examine()["C"]
         code = "{0:b}".format(int(C)).zfill(2)
-        return (not bool(int(code[0]))), (bool(int(code[1]))) # locked, remote
-
+        return (not bool(int(code[0]))), (bool(int(code[1])))  # locked, remote
 
     def setHeaterAndGas(self, heater_auto, gas_auto):
         # these values MUST be booleans! TODO: implement check
 
-        commandcode = int( str(int(gas_auto)) + str(int(heater_auto)), 2)
+        commandcode = int(str(int(gas_auto)) + str(int(heater_auto)), 2)
         # the way it works is An, where n is a decimal number, whos first bit (2^0) represents if heater is auto, and second bit represents if gas flow is auto
-        self.write("$A"+str(commandcode))
+        self.write("$A" + str(commandcode))
 
     def getHeaterAndGas(self):
         A = self.Examine()["A"]
@@ -284,7 +287,7 @@ class Instrument(Tool.MeasInstr):
         return (bool(int(code[0]))), (bool(int(code[1])))  # heater_auto, gas_auto
 
     def setAutoPID(self, AutoPID):
-        self.write("$L"+str(int(AutoPID)))
+        self.write("$L" + str(int(AutoPID)))
 
     def getAutoPID(self):
         L = self.Examine()["L"]
@@ -292,7 +295,7 @@ class Instrument(Tool.MeasInstr):
 
     def setHeaterSensor(self, sensor):
         if 1 <= int(sensor) <= 3:
-            self.write("$H"+str(int(sensor)))
+            self.write("$H" + str(int(sensor)))
         else:
             print("Invalid sensor")
 
@@ -301,9 +304,9 @@ class Instrument(Tool.MeasInstr):
         return int(H)
 
     def setPID(self, P, I, D):
-        self.write("$P"+str(P))
-        self.write("$I"+str(I))
-        self.write("$D"+str(D))
+        self.write("$P" + str(P))
+        self.write("$I" + str(I))
+        self.write("$D" + str(D))
 
     def getPID(self):
         return float(self.ask("R8")[1:]), float(self.ask("R9")[1:]), float(self.ask("R10")[1:])
@@ -318,18 +321,16 @@ class Instrument(Tool.MeasInstr):
         resp, A = resp.split("A")
 
         return {
-            "X":int(resp.strip("X")),
-            "A":int(A),
-            "C":int(C),
-            "S":int(S),
-            "H":int(H),
-            "L":int(L)
+            "X": int(resp.strip("X")),
+            "A": int(A),
+            "C": int(C),
+            "S": int(S),
+            "H": int(H),
+            "L": int(L)
         }
 
-
-
     def setDesiredTemperature(self, temperature):
-        self.write("$T"+str(round(float(temperature),3)))
+        self.write("$T" + str(round(float(temperature), 3)))
 
     def getDesiredTemperature(self):
         return float(self.ask("R0")[1:])
@@ -337,16 +338,16 @@ class Instrument(Tool.MeasInstr):
     def setSweep(self, line, PointTemperature, SweepTime, HoldTime):
         line = int(line)
         if (line >= 1) and (line <= 16):
-            x = self.ask("x"+str(line))
+            x = self.ask("x" + str(line))
             if line not in x:
                 print("Error writing line:" + line)
                 return False
 
-            values = [round(float(PointTemperature),3), round(float(SweepTime),3), round(float(HoldTime),3)]
-            i=1
+            values = [round(float(PointTemperature), 3), round(float(SweepTime), 3), round(float(HoldTime), 3)]
+            i = 1
             for x in values:
                 # point temperature
-                self.write("y"+str(i))
+                self.write("y" + str(i))
                 y = self.read()
                 if y not in 'y':
                     print("Error writing y:" + y)
@@ -364,27 +365,27 @@ class Instrument(Tool.MeasInstr):
     def readSweep(self, line):
         line = int(line)
         if (line >= 1) and (line <= 16):
-            self.write("x"+str(line))
+            self.write("x" + str(line))
             err = self.read()
             if 'x' != err[:1]:
-                print("Error writing x: "+err)
+                print("Error writing x: " + err)
 
             values_r = ["Point Temperature", "Sweep Time", "Hold Time"]
             values_w = [None] * 3
             for i in range(3):
-                self.write("y"+str(i+1))
+                self.write("y" + str(i + 1))
                 err = self.read()
                 if 'y' != err[:1]:
                     print("Error writing y: " + err)
 
                 self.write("r")
                 ret = self.read()
-                if '?' == ret[:1]: #command error'd
+                if '?' == ret[:1]:  # command error'd
                     print("Error reading value: " + ret)
                 else:
                     values_w[i] = ret[1:]
             for values in list(zip(values_r, values_w)):
-                print(values[0]+": "+values[1])
+                print(values[0] + ": " + values[1])
             return [float(i) for i in values_w]
             """ try: Possible catch for return variable just in case
                 return [float(i) for i in values_w]
@@ -401,7 +402,7 @@ class Instrument(Tool.MeasInstr):
                 return ret_arr"""
         else:
             print("Invalid sweep line")
-            return [None]*3
+            return [None] * 3
 
     def wipeSweep(self):
         self.write("w")
@@ -413,6 +414,7 @@ class Instrument(Tool.MeasInstr):
 
     def startSweep(self):
         self.write("$S1")
+
     def stopSweep(self):
         self.write("$S0")
 
@@ -425,10 +427,10 @@ class Instrument(Tool.MeasInstr):
 
         if S == 0:
             return (False, -1)
-        elif S%2 == 0:
-            return (True, S/2)
+        elif S % 2 == 0:
+            return (True, S / 2)
         else:
-            return (False, (S+1)/2)
+            return (False, (S + 1) / 2)
 
     def version(self):
         return self.ask("V")
@@ -442,24 +444,24 @@ class Instrument(Tool.MeasInstr):
             #                        print(idx)
             idx = idx + 1
             answer = self.read()
-            #print(answer)
-            if answer and answer is not "?*IDN?": #bug fix for when first connected, ?*IDN? is still in read buffer
+            # print(answer)
+            if answer and answer is not "?*IDN?":  # bug fix for when first connected, ?*IDN? is still in read buffer
                 is_empty = False
         return answer
+
 
 if (__name__ == '__main__'):
     i = Instrument("GPIB0::24")
     i.setControl(False, True)
 
-    i.setSweep(1,100,10,1)
+    i.setSweep(1, 100, 10, 1)
     i.readSweep(1)
     i.wipeSweep()
     i.readSweep(1)
-    while True: #yuck
+    while True:  # yuck
         cmd = input("Enter command: ")
         print(i.query(cmd))
     i.close()
-
 
 #   print(i.write('R1'))
 #   is_empty = True
@@ -472,7 +474,7 @@ if (__name__ == '__main__'):
 #       if answer:
 #           is_empty = False
 #  print(answer)
-            
+
 #    print(i.measure('T1'))
 #    print(i.measure('T3'))
 #    print(i.measure('T2'))
@@ -488,18 +490,14 @@ if (__name__ == '__main__'):
 #    print(i.measure('T1'))
 
 
-
-
-
-
-#    term_char = '\r' 
+#    term_char = '\r'
 #    rm = visa.ResourceManager()
 #    i = rm.get_instrument("GPIB0::24")
 #    i.write("$C3" + term_char)
 #    i.write('R3' + term_char)
 #    i.read()
 #    i.close()
-    #print(i.ask('V\r'))
-    #i.write('$R2\r')
-    #a = i.read()
-    #print(a)
+# print(i.ask('V\r'))
+# i.write('$R2\r')
+# a = i.read()
+# print(a)
