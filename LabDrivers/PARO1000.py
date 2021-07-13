82f0a0e07b4ff8d8c4d52d0020759ca581c2608b
sujathavkrishnan <sujathavkpillai@gmail.com>
2021-07-13 14:55:12 -0400

    Fixed PARO1000 I/O errors. Possibly caused by underlying bug in MeasInstr under INTF_SERIAL. Further investigation is required

--- LabDrivers/PARO1000.py
+++ LabDrivers/PARO1000.py
@@ -15,6 +15,21 @@ param = {'PRESSURE': 'psi', 'TEMPERATURE': 'C'}
 
 INTERFACE = Tool.INTF_SERIAL
 
+properties = {
+    'Units': {
+        'type':'selection',
+        'range':[
+            'psi',
+            'mbar or hPa',
+            'bar',
+            'kPa',
+            'MPa',
+            'in Hg',
+            'mm Hg or torr',
+            'm H20'
+        ]
+    }
+}
 
 class Instrument(Tool.MeasInstr):
 
@@ -33,7 +48,17 @@ class Instrument(Tool.MeasInstr):
         super(Instrument, self).__init__(resource_name,
                                          'PARO1000', debug=debug,
                                          interface=interface,
-                                         term_chars='\r\n', **kwargs)
+                                         term_chars='', baudrate=9600, parity='N', timeout=1, **kwargs)
+    def write(self, msg):
+        return self.connection.write((msg+'\r\n').encode())
+    def write_multiple(self, msgs):
+        return self.connection.write(('\r\n'.join(msgs)+'\r\n').encode())
+    def read(self):
+        return self.connection.readline().decode()
+    def ask(self, msg):
+        self.write(msg)
+        return self.read()
+
 
     def measure(self, channel='PRESSURE'):
         if channel in self.last_measure:
@@ -53,6 +78,33 @@ class Instrument(Tool.MeasInstr):
             answer = None
         return answer
 
+    def get(self):
+        ret = {}
+        if not self.DEBUG:
+            for channel in properties.keys():
+                if channel == 'Units':
+                    self.write('*0100UN')
+                    resp = self.read()
+                    ret['Units'] = properties['Units']['range'][int(resp[8:])-1]
+
+        return ret
+
+    def set(self, data):
+        if not self.DEBUG:
+            for channel, value in data.items():
+                if channel == 'Units':
+                    try:
+                        idx = properties['Units']['range'].index(value)
+                    except:
+                        print("Invalid unit selection for PARO1000")
+                        continue
+
+                    try:
+                        self.write_multiple(['*0100EW','*0100UN=%d'%(idx+1)])
+                    except:
+                        print("Unable to write to PARO1000")
+        else:
+            print("Debug mode enabled, PARO1000 property data: ", data)
     def identify(self, msg=''):
         if not self.DEBUG:
 
@@ -67,5 +119,11 @@ class Instrument(Tool.MeasInstr):
 
 
 if __name__ == "__main__":
-    i = Instrument("COM4")
-    print(i.identify())
+    i = Instrument("COM3")
+    i.get()
+    #print("writing")
+    #i.connection.write('*0100EW\r\n*0100UN=1\r\n'.encode())
+    #i.connection.write('*0100P3\r\n'.encode())
+    #print("reading")
+    #print(i.connection.readline().decode())
+    #print(i.identify())
